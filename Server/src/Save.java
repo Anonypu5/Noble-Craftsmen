ce09c9ecf1bf4e0d5953c13c2f8944bb4d92b5d8
Anonypu5 <elias.fyksen@grabit.no>
2015-05-23 23:36:37 +0200

    Shit

---
+++ Server/src/Save.java
@@ -0,0 +1,45 @@
+
+import java.io.File;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.FileHandler;
+
+/**
+ * Created by Elias on 5/23/2015.
+ */
+public class Save {
+
+    public static String osd, os, folder;
+
+    public Save(){
+        List<String> list = new ArrayList<String>();
+        Object a = list;
+
+        os = System.getProperty("os.name");
+        Console.println("Running on platform using "+os);
+
+        if(os.startsWith("Windows")){
+            osd = "\\";
+            folder = System.getProperty("user.home")+"\\AppData\\Roaming\\.NC-Server";
+        }else if(os.startsWith("Mac")){
+            osd = "/";
+            folder = System.getProperty("user.home")+"/NC-Server";
+        }else {
+            Console.println("NC-Server Does not currently support this platform");
+        }
+
+        File file = new File(folder);
+        if(!file.exists()){
+            Console.println("Could not find any directory at \""+folder+"\", would you like to create or set other directory path: create/other?");
+            String answer = Console.requestNext();
+            while(!answer.equals("create")&&!answer.equals("other")){
+                Console.println("Please write other or create");
+                answer = Console.requestNext();
+            }
+        }
+    }
+
+}class file implements Serializable {
+    static final long serialVersionUID = 1L;
+}
