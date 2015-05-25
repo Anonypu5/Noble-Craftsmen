02527ab190881a908bd35f716d805b2765bfbf0b
Ole Strohm <olestr@grabit.no>
2015-05-25 21:01:33 +0200

    FLERE FARGER :smile:

---
+++ Server/src/Theme.java
@@ -0,0 +1,60 @@
+import java.awt.*;
+import java.util.HashMap;
+
+/**
+ * Created by Ole on 25/05/2015.
+ */
+public class Theme {
+
+	public static HashMap<String, Theme> themes = new HashMap<>();
+
+	private String name;
+	private Color background, error, command, info;
+
+	public static void createTheme(String name, Color background, Color error, Color command, Color info) {
+		Theme theme = new Theme(name, background, error, command, info);
+
+		if(!themes.containsKey(name)){
+			themes.put(name, theme);
+			Console.println("Successfully created a theme with the name \"" + name + "\"");
+		} else {
+			Console.printErr("A theme with the name \"" + name + "\" already exists");
+		}
+	}
+
+	private Theme(String name, Color background, Color error, Color command, Color info) {
+		this.name = name;
+		this.background = background;
+		this.error = error;
+		this.command = command;
+		this.info = info;
+	}
+
+	public static Theme getTheme(String name) {
+		if(themes.containsKey(name)) {
+			return themes.get(name);
+		}
+		return null;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Color getBackground() {
+		return background;
+	}
+
+	public Color getError() {
+		return error;
+	}
+
+	public Color getCommand() {
+		return command;
+	}
+
+	public Color getInfo() {
+		return info;
+	}
+
+}
