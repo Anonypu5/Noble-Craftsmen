80868bcbc3cf7457c5ede3120fc1f90288474b3b
Ole Strohm <olestr@grabit.no>
2015-05-25 17:23:03 +0200

    Revert "Revert "mhm""
    This reverts commit 8ff110a2e38aadd66989f673e8ec230dfe49c124.

--- NC-Game/src/no/grabit/NobleCraftsmen/graphics/Texture.java
+++ NC-Game/src/no/grabit/NobleCraftsmen/graphics/Texture.java
@@ -68,4 +68,8 @@ public class Texture {
 		glBindTexture(GL_TEXTURE_2D, 0);
 	}
 
+	public void exit() {
+		glDeleteTextures(textureID);
+	}
+
 }
---
+++ NC-Launcher/src/no/grabit/NCLauncher/Launcher.java
@@ -0,0 +1,8 @@
+package no.grabit.NCLauncher;
+
+/**
+ * Created by Ole on 25/05/2015.
+ */
+public class Launcher {
+
+}
