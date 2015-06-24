3368c3f914a25dbd6d61db129dcd9c2a61d6ad7c
Anonypu5 <elias.fyksen@grabit.no>
2015-06-24 18:49:22 +0200

    shit i dont know

--- Server/src/Main.java
+++ Server/src/Main.java
@@ -1,10 +1,29 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 /**
  * Created by Elias on 5/23/2015.
  */
 
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://10.246.17.21:3306/grabit_no", "grabsit_no", "asd123");
+            System.out.println("shit");
+//            PreparedStatement statement = con.prepareStatement("select * from Noble_Craftsmen");
+//            ResultSet result = statement.executeQuery();
+//            while(result.next()){
+//                System.out.println(result.getString(1) + "    " + result.getString(2));
+//            }
+        }catch (Exception e){
+            System.out.println(e);
+            e.getStackTrace();
+        }
         new Console();
         Console.println("Console started successfully");
         new Save();
